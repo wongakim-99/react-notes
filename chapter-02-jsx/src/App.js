8fc7b17509cfcd6d86ada40eed5e4cac3670d577
wongakim-99 <202021000@sangmyung.kr>
2024-11-29 01:00:56 +0900

    feat : "JSX 기초 문법 실습"
    Related to : #2

--- chapter-02-jsx/src/App.js
+++ chapter-02-jsx/src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import {Fragment} from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const name = "김가원";
+    return(
+        <>
+            <h1>{name} Hello!</h1>
+            <h2>잘 작동함?</h2>
+        </>
+    )
 }
 
 export default App;
