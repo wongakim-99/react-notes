16fd7536364ac80a87b5b59d2c9133dd867d8e49
wongakim-99 <202021000@sangmyung.kr>
2024-11-30 16:37:23 +0900

    feat : "인라인 스타일링"
    style 객체를 미리 선언하고 div의 style 값으로 지정
    Related to : #2

--- chapter-02-jsx/src/App.js
+++ chapter-02-jsx/src/App.js
@@ -1,8 +1,20 @@
 import React from 'react';
+import './App.css';
 
 function App() {
-    const name = "리액트";
-    return <div>{name === '리액트' && <h1>리액트임.</h1>}</div>
+    const name = '리액트';
+    const style = {
+        backgroundColor: '#fff',
+        color: 'aqua',
+        fontSize: '48px',
+        fontWeight: 'bold',
+        padding: 16,
+    }
+    return (
+        <div style={style}>
+            {name}
+        </div>
+    );
 }
 
 export default App;
