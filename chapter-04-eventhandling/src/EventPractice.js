8deb1b81a7e4d65fa7a1ab9fc6e4c033a1dad214
wongakim-99 <202021000@sangmyung.kr>
2024-12-06 21:35:00 +0900

    feat : "스프레드 연산자 활용한 이벤트 예제"
    Related to : #4

--- chapter-04-eventhandling/src/EventPractice.js
+++ chapter-04-eventhandling/src/EventPractice.js
@@ -1,19 +1,32 @@
 import React, {useState} from 'react';
 
 const EventPractice = () => {
-    const [username, setUsername] =useState('');
-    const [message, setMessage] = useState('');
-    const onChangeUsername = e => setUsername(e.target.value);
-    const onChangeMessage = e => setMessage(e.target.value);
+    const [form, setForm] = useState({
+        username: "",
+        message: "",
+    });
 
+    const {
+        username,
+        message
+    } = form;
+    const onChange = e => {
+        const nextForm = {
+            ...form,  // 기존의 form 내용을 이 자리에 복사한 뒤
+            [e.target.name] : e.target.value  // 원하는 값을 덮어 씌우기
+        };
+        setForm(nextForm);
+    };
     const onClick = () => {
         alert(username + " : " + message);
-        setUsername(username);
-        setMessage("");
+        setForm({
+            username: "",
+            message: ""
+        });
     };
 
     const onKeyPress = e => {
-        if (e.key === 'Enter') {
+        if (e.key === "Enter") {
             onClick();
         }
     };
@@ -26,14 +39,14 @@ const EventPractice = () => {
                 name="username"
                 placeholder="사용자명"
                 value={username}
-                onChange={onChangeUsername}
+                onChange={onChange}
             />
             <input
                 type="text"
                 name="message"
                 placeholder="아무거나 입력ㄱㄱ"
                 value={message}
-                onChange={onChangeMessage}
+                onChange={onChange}
                 onKeyPress={onKeyPress}
             />
             <button onClick={onClick}>확인</button>
