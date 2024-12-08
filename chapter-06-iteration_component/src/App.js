c04cb227e8d5401a8fb06b19bf368c3d86810178
wongakim-99 <202021000@sangmyung.kr>
2024-12-08 21:32:57 +0900

    feat : "반복되는 컴포넌트 처리"
    파일 기본 세팅
    Related to : #4

---
+++ chapter-06-iteration_component/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import IterationSample from "./IterationSample";
+
+const App = () => {
+    return (
+        <div>
+          <IterationSample/>
+        </div>
+    );
+};
+
+export default App;
