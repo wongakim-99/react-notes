553ddfebfda7ae278e6983ff0fb0442bb0ac3cb3
wongakim-99 <202021000@sangmyung.kr>
2024-12-08 21:32:57 +0900

    feat : "반복되는 컴포넌트 처리"
    파일 기본 세팅
    Related to : #5

---
+++ chapter-06-iteration_component/src/IterationSample.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const IterationSample = () => {
+    return (
+        <div>
+            <li>김가원</li>
+            <li>허준</li>
+            <li>김영웅</li>
+            <li>김진교</li>
+        </div>
+    );
+};
+
+export default IterationSample;
