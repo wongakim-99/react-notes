401e56a64e0ff20f3b4a73187185955cabe05305
wongakim-99 <202021000@sangmyung.kr>
2024-12-10 16:26:17 +0900

    feat : "파일 변경사항 테스트"
    Related to : #5

--- chapter-06-iteration_component/src/IterationSample.js
+++ chapter-06-iteration_component/src/IterationSample.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const IterationSample = () => {
-    const names = ['김진교', '김가원', '허준', '김영웅']
+    const names = ['김진교', '김가원', '허준', '김영웅', '상무기']
     const nameList = names.map(name => <li>{name}</li>)
     return (
         <ul>{nameList}</ul>
