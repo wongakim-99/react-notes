c6be1968f114a34e916385eafa3eaabd3764b447
wongakim-99 <202021000@sangmyung.kr>
2024-12-14 20:19:03 +0900

    feat : "key 설정"
    index를 사용해서 각 배열에 key값 설정

--- chapter-06-iteration_component/src/IterationSample.js
+++ chapter-06-iteration_component/src/IterationSample.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const IterationSample = () => {
     const names = ['김진교', '김가원', '허준', '김영웅', '상무기']
-    const nameList = names.map(name => <li>{name}</li>)
+    const nameList = names.map((name, index) => <li key = {index}>{name}</li>)
     return (
         <ul>{nameList}</ul>
     );
