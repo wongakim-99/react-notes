d8ce82d478b945e75d90f9b9f0af0eedb01d21d5
wongakim-99 <202021000@sangmyung.kr>
2024-12-14 20:54:58 +0900

    feat : "데이터 추가 로직 업데이트"
    데이터를 추가함. concat 함수를 사용하여 배열의 불변성 유지
    Related to : #5

--- chapter-06-iteration_component/src/IterationSample.js
+++ chapter-06-iteration_component/src/IterationSample.js
@@ -1,10 +1,35 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const IterationSample = () => {
-    const names = ['김진교', '김가원', '허준', '김영웅', '상무기']
-    const nameList = names.map((name, index) => <li key = {index}>{name}</li>)
+    const [names, setNames] = useState([
+        {id : 1, text : "김영웅"},
+        {id : 2, text : "김가원"},
+        {id : 3, text : "김수민"},
+        {id : 4, text : "허준"},
+        {id : 5, text : "김승찬"}
+    ]);
+    const [inputText, setInputText] = useState('');
+    const [nextId, setNextId] = useState(6);  // 새로운 항목을 추가할 때 사용할 id
+
+    const onChange = e => setInputText(e.target.value);
+
+    const onClick = () => {
+        const nextNames = names.concat({
+            id : nextId,
+            text : inputText
+        });
+        setNextId(nextId + 1);
+        setNames(nextNames);
+        setInputText('');  // InputText 를 비운다.
+    }
+
+    const nameList = names.map((name) => <li key = {name.id}>{name.text}</li>)
     return (
-        <ul>{nameList}</ul>
+        <>
+            <input value={inputText} onChange={onChange}/>
+            <button onClick={onClick}>추가</button>
+            <ul>{nameList}</ul>
+        </>
     );
 };
 
