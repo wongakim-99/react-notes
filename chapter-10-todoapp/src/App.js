23e5db2c61e1ebd6d29b70d749d8b719693967cb
김가원(Kimgawon) <202021000@sangmyung.kr>
2025-01-04 12:13:36 +0900

    Feat : props를 통해 받아 온 배열을 활용
    TodoListItem으로 이루어진 배열로 변환하여 렌더링
    Related to : #7

--- chapter-10-todoapp/src/App.js
+++ chapter-10-todoapp/src/App.js
@@ -1,13 +1,31 @@
-import React from 'react';
+import React, {useState} from 'react';
 import TodoTemplate from "./components/TodoTemplate";
 import TodoInsert from "./components/TodoInsert";
 import TodoList from "./components/TodoList";
 
 const App = () => {
+    const [todos, setTodos] = useState([
+        {
+            id : 1,
+            text : "리액트의 기초 알아보기",
+            checked : true,
+        },
+        {
+            id : 2,
+            text : "스프링 부트 공부",
+            checked : false,
+        },
+        {
+            id : 3,
+            text : "스프링 부트 bean 에 대해서 공부",
+            checked : false,
+        }
+    ])
+
     return (
         <TodoTemplate>
             <TodoInsert/>
-            <TodoList/>
+            <TodoList todos={todos}/>
         </TodoTemplate>
     );
 };
